b772d6972dabb3c2dad2c690475b5aca8d038c08
mmarcy98 <36632158+mmarcy98@users.noreply.github.com>
2018-05-24 22:49:16 +0200

    Add files via upload

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Classes
+{
+    public class Vehicle
+    {
+        public int speed { get; set; }
+
+        public int Engine { get; set; }
+
+        public void StartEngine()
+        {
+
+        }
+    }
+
+    public class Motorcycle:Vehicle
+    {
+        public int wheels { get; set; }
+    }
+
+    public class Car:Vehicle
+    {
+        public int wheels { get; set; }
+    }
+
+    public class DieselCar : Car
+    {
+        public int loud { get; set; }
+
+        public void EngineCrash()
+        {
+            speed = 0;
+        }
+    }
+
+    public class BenyaCar : Car
+    {
+
+    }
+
+    public class ElectricCar :Car
+    {
+
+    }
+
+    public class Ship : Vehicle
+    {
+        public int MoveonWater { get; set; }
+    }
+
+    public class Truck: Vehicle
+    {
+
+    }
+
+    public class Bus : Vehicle
+    {
+
+    }
+
+
+}
